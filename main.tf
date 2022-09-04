3a63658c3ee2bf58c34d51f4fa0433762a85d435
dinushchathurya <dinushchathurya21@gmail.com>
2022-09-04 09:07:37 +0530

    feat: Create private subnet resource

--- main.tf
+++ main.tf
@@ -16,12 +16,26 @@ resource "aws_subent" "public" {
 
     vpc_id     = aws.vpc_id
     count      = lenght(var.public_subnets_cidr)
-    cidr_block = elements(var.public_subnets_cidr, count.index) 
+    cidr_block = element(var.public_subnets_cidr, count.index) 
     availability_zone = elements(var.availability_zones, count.index)
     map_public_ip_on_launch = true
     
     tags = { 
         Name        = "${var.project}-$element(var.availability_zones, count.index)-public-subnet"
+        Environment = "${var.environment}"
+    }
+}
+
+/* private subnet */
+resource "aws_subnet" "private" {
+
+    vpc_id     = aws_vpc.vpc.id
+    count      = length(var.private_subnets_cidr)
+    cidr_block = element(var.private_subnets_cidr, count.index)
+    availability_zone = element(var.availability_zones, count.index)
+
+    tags = {
+        Name        = "${var.project}-$element(var.availability_zones, count.index)-private-subnet"
         Environment = "${var.environment}"
     }
 }
